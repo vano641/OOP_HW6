e63c2b4660173cfb075fd0c26086416e9995680e
Ivan Mysovskii <vano641@mail.ru>
2024-12-05 17:22:04 +0300

    Создал пакет Модель и поместил в него файлы Пользователь, Студент, группа студентов

---
+++ model/Student.java
@@ -0,0 +1,28 @@
+package model;
+public class Student extends User implements Comparable<Student>{
+    
+    private Integer studentId;
+
+    public Student(Integer studentId, String firstName, String lastName, String middleName) {
+        super(firstName, lastName, middleName);
+        this.studentId = studentId;
+    }
+   
+    public Integer getStudentId() {
+        return this.studentId;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("studentId: %d, firstName: %s, lastName: %s, middleName: %s",
+        studentId, firstName, lastName, middleName);
+    }
+
+    @Override
+    public int compareTo(Student o) {
+// берем у текущего объекта его studentId и вызываем у него метод compareTo
+// у второго объекта также studentId
+        return this.studentId.compareTo(o.studentId);
+    }
+
+}
---
+++ model/StudentGroup.java
@@ -0,0 +1,46 @@
+package model;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import service.StudentGroupIterator; 
+public class StudentGroup implements Iterable<Student>{
+
+    private List<Student> studentList;
+
+
+    public StudentGroup() {
+        this.studentList = initStudents();
+    }
+    
+        public List<Student> initStudents(){
+        List<Student> StL = new ArrayList<>();
+        StL.add(new Student(1, "Иван", "Мысовский", "Андреевич"));
+        StL.add(new Student(2, "Кристина", "Мысовская", "Вячеславовна"));
+        StL.add(new Student(3, "Артем", "Мысовский", "Иванович"));
+        return StL;
+    }
+
+    public void PrintStudentList(){
+        for (Student i : studentList) {
+            System.out.println(String.format("studentId: %d, firstName: %s, lastName: %s, middleName: %s",
+                                         i.getStudentId(), i.getFirstName(), i.getLastName(), i.getMiddleName()));
+        }
+    }
+
+    public List<Student> getStudentList() {
+        return this.studentList;
+    }
+
+    @Override
+    public Iterator<Student> iterator() {
+        return new StudentGroupIterator(this); // метод итератора возвращает итератор
+    }
+
+// метод который помещает нового студента в список
+    public void createStudent(Integer studentId, String firstName, String lastName, String middleName) {
+        studentList.add(new Student(studentId, firstName, lastName, middleName));
+    }
+
+    
+}
