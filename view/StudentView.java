2453e015295d75a6b068260173c8c729f74bc223
Ivan Mysovskii <vano641@mail.ru>
2024-12-06 16:40:51 +0300

    Изменет вывод в консоль

--- view/StudentView.java
+++ view/StudentView.java
@@ -8,8 +8,9 @@ public class StudentView implements UserView<Student> {
     @Override
     public void sendOnConsole(List<Student> list) {
         for (Student i : list) {
-            System.out.println(i.getStudentId()); // выводим в консоль id студента
+            System.out.println(String.format("id:%d - firstName:%s", i.getStudentId(), i.getFirstName())); // выводим в консоль id студента
         }
+        System.out.println("");
     }
     
     
